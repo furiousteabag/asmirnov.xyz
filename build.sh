042ff313004f0ca1e7103fa2a3fa1cd9eda60cdb
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-29 01:18:21 +0530

    make more responsive

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ rm -rf $OUTPUT && mkdir -p $OUTPUT
 cp ./index.html $OUTPUT
 cp -r ./images $OUTPUT
 
-for FILE in ./content/*.md
-do
+for FILE in ./content/*.md; do
   FILENAME=$(basename -- "$FILE" .md)
   pandoc $FILE --template=./content/template.html -o "$OUTPUT/$FILENAME.html"
 done
