2ea398e731f24fb5061b64440908e8013fa9e76e
Alexander Smirnov <furiousteabag@gmail.com>
2023-11-10 15:19:47 +0530

    add plantuml support

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ OUTPUT="./out"
 rm -rf $OUTPUT && mkdir -p $OUTPUT
 
 cp ./index.html $OUTPUT
+
 cp -r ./images $OUTPUT
+find $OUTPUT/images -type f -name "*.puml" -o -name "*.plantuml" \
+    -exec plantuml -tsvg {} \;
 
 for FILE in ./content/*.md; do
   FILENAME=$(basename -- "$FILE" .md)
