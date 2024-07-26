03ae0da5b25438dcad006ac69977f257cdcc94e5
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-26 13:27:32 +0200

    upd date format lua script

--- utils/date-format.lua
+++ utils/date-format.lua
@@ -1,14 +1,21 @@
 function Meta(meta)
   if meta.date then
     local format = "(%d+)-(%d+)-(%d+)"
-    local y, m, d = pandoc.utils.stringify(meta.date):match(format)
-    local date = os.time({
-      year = y,
-      month = m,
-      day = d,
-    })
-    local date_string = os.date("%d %b %Y", date)
-    meta.date = pandoc.Str(date_string)
-    return meta
+    local date_string = pandoc.utils.stringify(meta.date)
+    local y, m, d = date_string:match(format)
+    if y and m and d then
+      local date = os.time({
+        year = y,
+        month = m,
+        day = d,
+      })
+      local formatted_date_string = os.date("%d %b %Y", date)
+      meta.date = pandoc.Str(formatted_date_string)
+    else
+      io.stderr:write("Error: Date format is incorrect or missing. Expected format: YYYY-MM-DD, got: " .. date_string .. "\n")
+    end
+  else
+    io.stderr:write("Error: Date metadata is missing.\n")
   end
+  return meta
 end
